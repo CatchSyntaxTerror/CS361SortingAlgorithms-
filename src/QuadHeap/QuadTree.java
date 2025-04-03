37ebbb064bef17d4e0af9141a5d73187bffee706
SyntaxTerror <yamin@unm.edu>
2025-04-03 09:53:49 -0600

    Node class complete, Quade-Tree almost complete

--- src/QuadHeap/QuadTree.java
+++ src/QuadHeap/QuadTree.java
@@ -4,6 +4,10 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * Author: Youssef Amin
+ * Quad-Heap implementation
+ */
 public class QuadTree {
     public class Node {
         private int value;
