b216c6675ae50b631b553e2387a01c1a4c18419a
SyntaxTerror <yamin@unm.edu>
2025-04-04 15:23:03 -0600

    Almost Done

--- src/QuadHeap/QuadTree.java
+++ src/QuadHeap/QuadTree.java
@@ -1,43 +1,11 @@
 package src.QuadHeap;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
-
 /**
  * Author: Youssef Amin
  * Quad-Heap implementation
  */
 public class QuadTree {
-    public class Node {
-        private int value;
-        Node first;
-        Node second;
-        Node third;
-        Node fourth;
-
-        public Node(int value) {
-            this.value = value;
-        }
-
-        public HashSet<Node> getLeftChildren() {
-            HashSet<Node> set = new HashSet<>();
-            if (first != null) set.add(first);
-            if (second != null) set.add(second);
-            return set;
-        }
-
-        public HashSet<Node> getRightChildren() {
-            HashSet<Node> set = new HashSet<>();
-            if (third != null) set.add(third);
-            if (fourth != null) set.add(fourth);
-            return set;
-        }
-    }
-
     private static Node root;
-
-    int[] exImp = new int[]{12, 10, 8, 6, 11, 9, 7, 4, 3, 5, 2, 1};
 
     /**
      * constructor for a whole array of data
@@ -51,85 +19,115 @@ public class QuadTree {
      * constructor with one value to create the root
      * @param value root nodes value
      */
-    public QuadTree(int value){
+    public QuadTree(int value) {
         root = new Node(value);
     }
 
-    /**
-     * adds a value to the tree
-     * @param value data to be added
-     */
-    public void add(int value) {
-        if (root == null) {
-            root = new Node(value);
-        } else {
-            add(root, value);
-        }
-    }
 
     /**
-     * recursively finds a leftmost null node to add
-     * @param value int to be added
+     * recursively and nodes from left to right
+     *
+     * @param node node to add
      */
-    private void add(Node node, int value) {
+    private Node insert(Node node, int value) {
+        if (node == null) {
+            return new Node(value);
+        }
         if (value < node.value) {
-            if(node.first == null) {
-                node.first = new Node(value);
-            } else if(node.second == null) {
-                node.second = new Node(value);
-            } else {
-                //Todo: alternate stepping first or second
+            if (node.first == null) {
+                node.first = insert(node.first, value);
+            } else if (value > node.first.value) {
+                node.second = insert(node.second, value);
             }
-        } else if (value > node.value) {
-            if(node.third == null) {
-                node.third = new Node(value);
-            } else if(node.fourth == null){
-                node.fourth = new Node(value);
-            } else {
-                //Todo: alternate stepping third or fourth
+        } else {
+            if (node.third == null) {
+                node.third = insert(node.third, value);
+            } else if (value > node.third.value) {
+                node.fourth = insert(node.fourth, value);
             }
+        }
+        return node;
+    }
+
+    /**
+     * fills the whole tree with an array of values
+     * @param values a given array
+     */
+    public void fill(int[] values) {
+        for (int val : values) {
+            root = insert(root, val);
         }
     }
 
-
     /**
-     * goes to the next left node
-     * @param current the current node
-     * @return the current nodes left child
+     * Thanks haskell ;)
+     * @param node the root node of the tree
+     * @return number of nodes in the tree
      */
-    private Node stepToFirst(Node current) {
-        return current.first;
-    }
-
-    private Node stepToSecond(Node current){
-        return current.second;
-    }
-
-    /**
-     * goes to the next right node
-     * @param current the current node
-     * @return the current nodes right child
-     */
-    private Node stepToThird(Node current) {
-        return current.third;
-    }
-
-    private Node stepToFourth(Node current){
-        return current.fourth;
-    }
-
-    /**
-     * adds all elements of the array to the tree
-     * @param data the set of integers
-     */
-    private void fill(int[] data){
-        for (int value: data) {
-            add(root, value);
+    public int getSize(Node node) {
+        if (node == null) {
+            return 0;
         }
+        return 1 + getSize(node.first)
+                + getSize(node.second)
+                + getSize(node.third)
+                + getSize(node.fourth);
     }
 
+    /**
+     * returns an array representation of the tree.
+     * remember, in this case:
+     * children would be 4n+1, 4n+2, 4n+3 and 4n+4, n = parent node
+     *
+     * @return the array of values
+     */
+    public int[] toArray() {
+        if (root == null) {
+            return new int[0];
+        }
+        //Todo: figure out logic here
+        return null;
+    }
+
+    /**
+     * balancing the tree will make it sooooo much better
+     */
+    private void balance() {
+        //Todo: write rotation methods and balance logic, return true if
+        // successfully balanced;
+    }
+
+
+    /**
+     * this main method is just for me to test the tree
+     * @param args nada
+     */
     public static void main(String[] args) {
+        int[] exImp = new int[]{12, 10, 8, 6, 11, 9, 7, 4, 3, 5, 2, 1};
 
     }
 
+    public class Node {
+        private int value;
+        Node first;
+        Node second;
+        Node third;
+        Node fourth;
+
+        public Node(int value) {
+            this.value = value;
+        }
+
+        /**
+         * string representation of a node. for debugging purposes.
+         *
+         * @return node as string with children
+         */
+        public String nodeToString() {
+            return "[" + value + "], Children: 1. [" + first + "], " +
+                    "2. [" + second + "], 3. [" + third + "], 4. [" + fourth + "]";
+        }
+    }
 }
+
+
