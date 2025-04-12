494cd10085a37b09e4cb9e95b9ae0ac26fd5ea9e
Nickolas Chacon <nchacon77@unm.edu>
2025-04-12 12:03:23 -0600

    Make generating and storing configs from 2^20-2^30 with integers and doubles.

--- src/GenerateData.java
+++ src/GenerateData.java
@@ -1,13 +1,62 @@
 package src;
 
-import java.util.random.*;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
 
+/**
+ * The GenerateData class generates config files from 2^20-2^30 with integers and doubles. You MUST specify
+ * in private static final String OUTPUT_DIR = "./"; where you want them to be stored before you run the class.
+ * As of now, the data will be made into text files varying from 4 megabytes to 4 gigabytes for the integer files.
+ * The double files are doubled so they range from 8 megabytes to 8 gigabytes. Make sure you have storage.
+ * Also, they will not be stored as arrays. They will just be random numbers with a new line between each one.
+ */
 public class GenerateData {
 
-    public void run(int dataSize){
-        switch(dataSize){
-            case 20:
+    private static final int START_EXP = 20;
+    private static final int END_EXP = 30;
+    private static final Random rand = new Random();
 
+    //TODO: Change this to the directory where you want the files to be stored at(between the " ")
+    private static final String OUTPUT_DIR = "./";
+
+    public static void main(String[] args) {
+        //integers
+        for (int exp = START_EXP; exp <= END_EXP; exp++) {
+            long size = 1L << exp;//Bit shifting 2^exp val (long stores up to 2^63)
+            String filename = OUTPUT_DIR + "ints_2^" + exp + ".txt";
+
+            System.out.println("Generating file: " + filename + " with " + size + " integers");
+
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
+                for (long i = 0; i < size; i++) {
+                    writer.write(rand.nextInt() + "\n");
+                }
+            } catch (IOException e) {
+                System.err.println("Error writing file: " + filename);
+            }
+            System.out.println("Finished generating: " + filename);
         }
+        System.out.println("All integer files generated.");
+
+        //doubles
+        for (int exp = START_EXP; exp <= END_EXP; exp++) {
+            long size = 1L << exp;
+            String filename = OUTPUT_DIR + "doubles_2^" + exp + ".txt";
+
+            System.out.println("Generating file: " + filename + " with " + size + " doubles");
+
+            try (BufferedWriter writer = new BufferedWriter(new FileWriter(filename))) {
+                for (long i = 0; i < size; i++) {
+                    writer.write(rand.nextDouble() + "\n");
+                }
+            } catch (IOException e) {
+                System.err.println("Error writing file: " + filename);
+            }
+            System.out.println("Finished generating: " + filename);
+        }
+        System.out.println("All double files generated.");
     }
 }
+
