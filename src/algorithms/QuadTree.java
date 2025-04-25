6062a0bce5bdd3dca3e3ffbc863a62c144ec1b2e
SyntaxTerror <yamin@unm.edu>
2025-04-25 15:50:01 -0600

    Refactored QuadTree, Will be finished this weekend

--- src/algorithms/QuadTree.java
+++ src/algorithms/QuadTree.java
@@ -1,64 +1,51 @@
 package algorithms;
 
+import javax.swing.*;
+import java.util.Arrays;
+
 /**
  * Author: Youssef Amin
  * Quad-Heap implementation
  */
 public class QuadTree {
     public static Node root;
-    public int[] treeArray;
 
     /**
-     * constructor for a whole array of data
-     * @param arr Data set, right now only int arrays
+     * constructor for tree
+     * @param values array of integers
      */
-    public QuadTree(int[] arr) {
-        fill(arr);
-        treeArray = new int[getSize(root)];
-        //Todo: constructor not right
+    public QuadTree(int[] values ){
+        root = new Node(0);
+        fill(values, root, 0);
     }
 
-    /**
-     * constructor with one value to create the root
-     * @param value root nodes value
-     */
-    public QuadTree(int value) {
-        root = new Node(value);
-    }
-
-
-    /**
-     * recursively and nodes from left to right
-     *
-     * @param node node to add
-     */
-    private Node insert(Node node, int value) {
-        if (node == null) {
-            return new Node(value);
-        }
-        if (value < node.value) {
-            if (node.first == null) {
-                node.first = insert(node.first, value);
-            } else if (value > node.first.value) {
-                node.second = insert(node.second, value);
-            }
-        } else {
-            if (node.third == null) {
-                node.third = insert(node.third, value);
-            } else if (value > node.third.value) {
-                node.fourth = insert(node.fourth, value);
-            }
-        }
-        return node;
-    }
 
     /**
      * fills the whole tree with an array of values
      * @param values a given array
      */
-    public void fill(int[] values) {
-        for (int val : values) {
-            root = insert(root, val);
+    public void fill(int[] values, Node node, int i) {
+        node.value = values[i]; // set this node's value
+
+        if (4 * i + 1 < values.length) {
+            Node firstChild = new Node(0); // placeholder value
+            node.setFirst(firstChild);
+            fill(values, firstChild, 4 * i + 1);
+        }
+        if (4 * i + 2 < values.length) {
+            Node secondChild = new Node(0);
+            node.setSecond(secondChild);
+            fill(values, secondChild, 4 * i + 2);
+        }
+        if (4 * i + 3 < values.length) {
+            Node thirdChild = new Node(0);
+            node.setThird(thirdChild);
+            fill(values, thirdChild, 4 * i + 3);
+        }
+        if (4 * i + 4 < values.length) {
+            Node fourthChild = new Node(0);
+            node.setFourth(fourthChild);
+            fill(values, fourthChild, 4 * i + 4);
         }
     }
 
@@ -86,32 +73,52 @@ public class QuadTree {
      * @return the array of values
      */
     public int[] getTreeArray() {
-        if (root == null) {
-            return new int[0];
-        }
-        //Todo: heapify and just pop off the first element each time
-        return null;
+        int size = getSize(root);
+        int[] arr = new int[size];
+        fillArray(root, arr, 0);
+        return arr;
+    }
+
+    /**
+     * this method is just a helper for getTreeArray()
+     * @param node current node
+     * @param arr array to fill
+     * @param i index
+     */
+    private void fillArray(Node node, int[] arr, int i) {
+        if (node == null || i >= arr.length) return;
+        arr[i] = node.value;
+
+        fillArray(node.first, arr, 4 * i + 1);
+        fillArray(node.second, arr, 4 * i + 2);
+        fillArray(node.third, arr, 4 * i + 3);
+        fillArray(node.fourth, arr, 4 * i + 4);
     }
 
 
     public int DFS(Node node, int value){
-        if(node != null && value == node.value) return value;
-        DFS(node.first, value);
-        DFS(node.second, value);
-        DFS(node.third, value);
-        DFS(node.fourth, value);
+        if (node == null) return Integer.MAX_VALUE;
+        if (node.value == value) return value;
+
+        int found = DFS(node.first, value);
+        if (found != Integer.MAX_VALUE) return found;
+
+        found = DFS(node.second, value);
+        if (found != Integer.MAX_VALUE) return found;
+
+        found = DFS(node.third, value);
+        if (found != Integer.MAX_VALUE) return found;
+
+        found = DFS(node.fourth, value);
+        if (found != Integer.MAX_VALUE) return found;
+
         return Integer.MAX_VALUE;
     }
 
     public void minHeapSort(Node node){
-        Node[] children = new Node[]{node.first, node.second, node.third, node.fourth};
-        for (int i = 0; i < children.length; i++) {
-            if(children[i] == null) return;
-            if(children[i].value < node.value){
-                swapValues(children[i], node);
-            }
-            minHeapSort(children[i]);
-        }
+        //Todo:  need:
+        //heapify(Node node), to push down larger nodes.
+        //minHeapSort() call heapify() starting from the bottom of the tree upward.
     }
 
     private void swapValues(Node fir, Node sec){
@@ -120,24 +127,38 @@ public class QuadTree {
         sec.value = temp;
     }
 
-    /**
-     * this main method is just for me to test the tree
-     * @param args nada
-     */
-    public static void main(String[] args) {
-        int[] exImp = new int[]{12, 10, 8, 6, 11, 9, 7, 4, 3, 5, 2, 1};
-
-    }
-
-    public class Node {
-        private int value;
+    public static class Node {
+        int value;
         Node first;
         Node second;
         Node third;
         Node fourth;
 
+        Node[] children = new Node[4];
+
         public Node(int value) {
             this.value = value;
+        }
+
+        //setters
+        public void setFirst(Node node) {
+            this.first = node;
+            this.children[0] = node;
+        }
+
+        public void setSecond(Node node) {
+            this.second = node;
+            this.children[1] = node;
+        }
+
+        public void setThird(Node node) {
+            this.third = node;
+            this.children[2] = node;
+        }
+
+        public void setFourth(Node node) {
+            this.fourth = node;
+            this.children[3] = node;
         }
 
         /**
@@ -146,8 +167,7 @@ public class QuadTree {
          * @return node as string with children
          */
         public String nodeToString() {
-            return "[" + value + "], Children: 1. [" + first + "], " +
-                    "2. [" + second + "], 3. [" + third + "], 4. [" + fourth + "]";
+            return "[" + value + "]";
         }
     }
 }
